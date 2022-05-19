f0db0cc847ccdfaee4ead981449be82250c897be
richydany@gmail.com <richydany@gmail.com>
2022-05-19 21:11:48 +0200

    Finished the membership attack

--- membership_attack/membership_inference.py
+++ membership_attack/membership_inference.py
@@ -2,22 +2,45 @@ import torch
 from torch.utils.data import DataLoader, Subset
 
 from attacker.utils import get_model, get_dataset
-from membership_attack.utils import _path_generator, _indices_loader
+from membership_attack.utils import _path_generator, _indices_loader, _entropy_calculate
+from victim import OOD
+from victim.interface import fetch_logits
 
 
-def _entropy_calculate(log_its):
-    return torch.distributions.Categorical(torch.nn.Softmax(log_its)).entropy()
-
-
-def _attacker(attacker, data_type, indices_path, model_path):
+def _attacker(attacker, train_set, test_set, ood_dataset, indices_path, model_path):
     model = get_model(attacker, 10)
     model.load_state_dict(torch.load(_path_generator(model_path)))
 
     indices = _indices_loader(indices_path)
-    full_dataset = get_dataset(data_type)
-    indices = [i for i in range(len(full_dataset)) if i not in indices]
+    indices = [i for i in range(len(train_set)) if i not in indices]
+    train_set = Subset(train_set, indices)
 
-    dataset = Subset(full_dataset, indices)
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+    return {
+        "in_sample": [
+            _entropy_calculate(model(x)[0])
+            for d_set in [train_set, test_set]
+            for x, _ in DataLoader(d_set, batch_size=1, shuffle=False)
+        ],
+        "ood": [_entropy_calculate(model(x)[0]) for x, _ in DataLoader(ood_dataset, batch_size=1, shuffle=False)],
+    }
 
-    return [_entropy_calculate(model(x)) for x, _ in dataloader]
+
+def _victim(victim_args, train_set, test_set, ood_dataset):
+    return {
+        "in_sample": [
+            _entropy_calculate(fetch_logits(victim_args, x)[0])
+            for d_set in [train_set, test_set]
+            for x, _ in DataLoader(d_set, batch_size=1, shuffle=False)
+        ],
+        "ood": [_entropy_calculate(fetch_logits(victim_args, x)[0])
+                for x, _ in DataLoader(ood_dataset, batch_size=1, shuffle=False)],
+    }
+
+
+def generate_results(victim_args, attacker, indices_path, model_path, data_type):
+    train_set, test_set, outputs = get_dataset(data_type)
+    ood_dataset = get_dataset(OOD)
+    attacker_result = _attacker(attacker, train_set, test_set, ood_dataset, indices_path, model_path)
+    victim_result = _victim(victim_args, train_set, test_set, ood_dataset)
+
+    return attacker_result, victim_result
--- membership_attack/utils.py
+++ membership_attack/utils.py
@@ -1,6 +1,7 @@
 import os
 
 import torch
+from torch.distributions import Categorical
 
 
 def _indices_loader(indices_file):
@@ -9,3 +10,7 @@ def _indices_loader(indices_file):
 
 def _path_generator(model_file):
     return os.path.join(os.getcwd(), model_file)
+
+
+def _entropy_calculate(log_its):
+    return Categorical(torch.nn.Softmax(log_its)).entropy()
