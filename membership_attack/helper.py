438b893d6df7b711d48d6e7bf94a777ea5c248a9
richydany@gmail.com <richydany@gmail.com>
2022-05-19 10:15:01 +0200

    Made some cleanup

--- membership_attack/helper.py
+++ membership_attack/helper.py
@@ -1,8 +1,9 @@
-import torch
 import os
-import numpy as np
 
-from torrch.utils.data import Subset, DataLoader
+import numpy as np
+import torch
+from torch.utils.data import Subset, DataLoader
+
 
 def _fetch_dataset(_):
     pass
@@ -10,26 +11,25 @@ def _fetch_dataset(_):
 
 def _indices_generator(q_type, size, percentage):
     if q_type == 'random':
-        indices = np.random.default_rng().choice(
-                size,
-                size=size*percentage,
-                replace=False
-                )
-    elif q_type in ['coreset','coreset_cross'):
-        indices = _load_coreset()[:int(size*percentage)]
-    return indices
+        return np.random.default_rng().choice(
+            size,
+            size=size * percentage,
+            replace=False
+        )
+
+    elif q_type in ['coreset', 'coreset_cross']:
+        return None
+
 
 def _path_generator(model_file):
     return os.path.join(os.getcwd(), model_file)
+
 
 def generate_results(model_file):
     model = torch.load(_path_generator(model_file))
 
     # TODO: Fetch the query_percentage and query_type
-    _, attacker, victim, query_type, percentage = tuple(model_file.split("_"))
-    indices = _indices_generator(qery_type, dataloader.size(), percentage)
+    _, attacker, victim, q_type, perc = tuple(model_file.split("_"))
+    indices = _indices_generator(q_type, dataloader.size(), perc)
     dataset = Subset(_fetch_dataset(_), indices)
-    dataloader = DataLoader(dataset, batch_size=config['batch_size'], shuffle =False)
-
-
-    
+    dataloader = DataLoader(dataset, batch_size=config['batch_size'], shuffle=False)
