2bd7a26344c135da4820fa4153335299f82541bc
95-ag <96833386+95-ag@users.noreply.github.com>
2022-05-21 22:20:03 +0000

    tested scheduler +trial code for k-logits

--- attacker/config.py
+++ attacker/config.py
@@ -5,10 +5,12 @@ from victim.__init__ import *
 config = {
     "batch_size": 500,
     "learning_rate": 0.008,
-    "epochs": 80,
-    "query_size":10000,
-    "query_type": 'random',
+    "lr_cycles": 2,
+    "lr_steps": 1000,
+    "base_lr": 1e-5,
+    "max_lr": 1e-1,
+    "query_size":50000,
+    "query_type": 'coreset',
     "victim": { "data": CIFAR_10,  "model_name": RESNET50 },
-    "attacker": RESNET34,
-    "klogits": 0
+    "attacker": RESNET34
 }
--- attacker/utils.py
+++ attacker/utils.py
@@ -106,22 +106,34 @@ def get_model(model_name, outputs):
 
 # Train the models
 
-def training(model, train_loader, test_loader, epochs, optimizer, loss):
+def training(model, train_loader, test_loader):
+    # initialize score lists
     train_acc = []
     train_loss = []
     test_acc = []
     test_loss = []
     train_loss_func = None
+    
+    # select optimiser and scheduler
+    optimizer = torch.optim.Adam(model.parameters(), lr=config["learning_rate"])
+    scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=config["base_lr"], max_lr=config["max_lr"],
+                                                  step_size_up=config["lr_steps"], mode='triangular2', cycle_momentum=False)
+    
+    # select loss
+    loss = torch.nn.CrossEntropyLoss()
+    
+    # train for epochs
+    epochs = int(2*config["lr_cycles"]*config["lr_steps"]/(len(train_loader.dataset)/config["batch_size"]))
+    print(f'Total epochs: {epochs}')
     for epoch in range(epochs):
         print("\repoch", epoch + 1)
         
-        # Train Data
+        # train on Train Data
         num_train = 0
         num_correct_train = 0
         model.train() 
         for (xList, yList) in train_loader:
-            xList, yList = torch.autograd.Variable(
-                xList), torch.autograd.Variable(yList)
+            xList, yList = torch.autograd.Variable(xList), torch.autograd.Variable(yList)
             optimizer.zero_grad()
 
             if torch.cuda.is_available():
@@ -130,14 +142,16 @@ def training(model, train_loader, test_loader, epochs, optimizer, loss):
                 device = torch.device(DEVICE)
                 model.to(device)
             
+            # get outputs and train model
             with torch.set_grad_enabled(True):
                 outputs = model(xList)
                 train_loss_func = loss(outputs, yList)
                 train_loss_func.backward()
                 optimizer.step()
+                scheduler.step()
 
-            num_train += len(yList)  # i.e., add bath size
-
+            # get correct predictions count
+            num_train += len(yList)
             predicts = torch.max(outputs.data, 1)[1]
             num_correct_train += (predicts == yList).float().sum()
 
@@ -145,11 +159,9 @@ def training(model, train_loader, test_loader, epochs, optimizer, loss):
 
         train_acc.append(num_correct_train / num_train)
         train_loss.append(train_loss_func.data)
-        print("\r    - train_acc %.5f train_loss %.5f" %
-              (train_acc[-1], train_loss[-1]))
-        # if(epoch%(int(epochs/4))==0): print(model.L0[0].weight)
+        print("\r    - train_acc %.5f train_loss %.5f" % (train_acc[-1], train_loss[-1]))
 
-        # Test Data
+        # evaluate on Test Data
         num_test = 0
         num_correct_test = 0
         model.eval()
@@ -160,91 +172,120 @@ def training(model, train_loader, test_loader, epochs, optimizer, loss):
                 device = torch.device(DEVICE)
                 model.to(device)
             
+            #  get outputs
             with torch.set_grad_enabled(False):
                 outputs = model(xList)
                 test_loss_func = loss(outputs, yList)
-
+            
+            # get correct predictions count
             num_test += len(yList)
             predicts = torch.max(outputs.data, 1)[1]
             num_correct_test += (predicts == yList).float().sum()
 
         test_acc.append(num_correct_test / num_test)
         test_loss.append(test_loss_func.data)
-        print("\r    - test_acc  %.5f test_loss  %.5f" %
-              (test_acc[-1], test_loss[-1]))
+        print("\r    - test_acc  %.5f test_loss  %.5f" %(test_acc[-1], test_loss[-1]))
     return train_loss, train_acc, test_loss, test_acc
 
 
 # Train model with queried logits
 
-# + active=""
-# def attacker_training_logits(attacker, victim, train_loader, test_loader, epochs, optimizer, loss):
-#     train_acc = []
-#     train_loss = []
-#     test_acc = []
-#     test_loss = []
-#     train_loss_func = None
-#     for epoch in range(epochs):
-#         num_train = 0
-#         num_correct_train = 0
-#         print("\repoch", epoch + 1)
-#         # Train Data
-#         for (xList, _) in train_loader:
-#             # query from victim
-#             yList = fetch_logits(args=victim, query_img=xList)
-#             
-#             xList, yList = torch.autograd.Variable(
-#                 xList), torch.autograd.Variable(yList)
-#             optimizer.zero_grad()
-#
-#             if torch.cuda.is_available():
-#                 xList = xList.type(torch.cuda.FloatTensor)
-#                 yList = yList.type(torch.cuda.LongTensor)
-#                 device = torch.device(DEVICE)
-#                 attacker.to(device)
-#             
-#             attacker.train()    
-#             outputs = attacker(xList)
-#             train_loss_func = loss(outputs, yList)
-#             train_loss_func.backward()
-#             optimizer.step()
-#
-#             num_train += len(yList)  # i.e., add bath size
-#
-#             predicts = torch.max(outputs.data, 1)[1]
-#             num_correct_train += (predicts == yList).float().sum()
-#
-#             print('\r %d ...' % num_train, end='')
-#
-#         train_acc.append(num_correct_train / num_train)
-#         train_loss.append(train_loss_func.data)
-#         print("\r    - train_acc %.5f train_loss %.5f" %
-#               (train_acc[-1], train_loss[-1]))
-#         # if(epoch%(int(epochs/4))==0): print(model.L0[0].weight)
-#
-#         # Test Data
-#         num_test = 0
-#         num_correct_test = 0
-#         for (xList, yList) in test_loader:
-#             if torch.cuda.is_available():
-#                 xList = xList.type(torch.cuda.FloatTensor)
-#                 yList = yList.type(torch.cuda.LongTensor)
-#                 device = torch.device(DEVICE)
-#                 attacker.to(device)
-#
-#             attacker.eval()
-#             outputs = attacker(xList)
-#             test_loss_func = loss(outputs, yList)
-#
-#             num_test += len(yList)
-#             predicts = torch.max(outputs.data, 1)[1]
-#             num_correct_test += (predicts == yList).float().sum()
-#
-#         test_acc.append(num_correct_test / num_test)
-#         test_loss.append(test_loss_func.data)
-#         print("\r    - test_acc  %.5f test_loss  %.5f" %
-#               (test_acc[-1], test_loss[-1]))
-#     return train_loss, train_acc, test_loss, test_acc
+# +
+querytestloader =  query_victim(victim, outputs, testloader, len(testloader.dataset))
+sampledtrainloader = query_type(victim, outputs, trainloader, query_size,
+                                'queried/query_traindata_cifar10_resenet50', query_type)
+victim_model = fetch_victim_model(args=victim)
+
+def attacker_training_w_logits(victim_model, attacker_model, sampledtrainloader, querytestloader, k):
+    # initialize score lists
+    train_acc = []
+    train_loss = []
+    test_acc = []
+    test_loss = []
+    train_loss_func = None
+    
+    # select optimiser and scheduler
+    optimizer = torch.optim.Adam(attacker_model.parameters(), lr=config["learning_rate"])
+    scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=config["base_lr"], max_lr=config["max_lr"],
+                                                  step_size_up=config["lr_steps"], mode='triangular2', cycle_momentum=False)
+
+    # select loss
+    loss = torch.nn.CrossEntropyLoss()
+    
+    # train for epochs
+    epochs = int(2*config["lr_cycles"]*config["lr_steps"]/(len(train_loader.dataset)/config["batch_size"]))
+    print(f'Total epochs: {epochs}')
+    for epoch in range(epochs):
+        print("\repoch", epoch + 1)
+        
+        # train on Train Data
+        num_train = 0
+        num_correct_train = 0
+        attacker_model.train() 
+        for (xList, _) in sampledtrainloader:
+            xList = torch.autograd.Variable(xList)
+            optimizer.zero_grad()
+
+            if torch.cuda.is_available():
+                xList = xList.type(torch.cuda.FloatTensor)
+                device = torch.device(DEVICE)
+                attacker_model.to(device)
+            
+            # query victim for logits
+            yList = victim_model(xList)
+            val, ind = torch.topk(yList, k, dim=1)
+            ones = (torch.ones(yList.shape)*float('-inf'))
+            if torch.cuda.is_available():
+                ones = ones.type(torch.cuda.FloatTensor)
+            yList = ones.scatter_(1, ind, val)
+            yList = torch.nn.functional.softmax(yList, dim=1)
+            
+            # get outputs and train model
+            with torch.set_grad_enabled(True):
+                outputs = attacker_model(xList)
+                train_loss_func = loss(outputs, yList)
+                train_loss_func.backward()
+                optimizer.step()
+                scheduler.step()
+
+            # get correct predictions count
+            num_train += len(yList)
+            predicts = torch.max(outputs.data, 1)[1]
+            num_correct_train += (predicts == yList).float().sum()
+
+            print('\r %d ...' % num_train, end='')
+
+        train_acc.append(num_correct_train / num_train)
+        train_loss.append(train_loss_func.data)
+        print("\r    - train_acc %.5f train_loss %.5f" % (train_acc[-1], train_loss[-1]))
+
+        # evaluate on Test Data
+        num_test = 0
+        num_correct_test = 0
+        attacker_model.eval()
+        for (xList, yList) in queriedtestloader:
+            if torch.cuda.is_available():
+                xList = xList.type(torch.cuda.FloatTensor)
+                yList = yList.type(torch.cuda.LongTensor)
+                device = torch.device(DEVICE)
+                attacker_model.to(device)
+            
+            #  get outputs
+            with torch.set_grad_enabled(False):
+                outputs = attacker_model(xList)
+                test_loss_func = loss(outputs, yList)
+            
+            # get correct predictions count
+            num_test += len(yList)
+            predicts = torch.max(outputs.data, 1)[1]
+            num_correct_test += (predicts == yList).float().sum()
+
+        test_acc.append(num_correct_test / num_test)
+        test_loss.append(test_loss_func.data)
+        print("\r    - test_acc  %.5f test_loss  %.5f" %(test_acc[-1], test_loss[-1]))
+    return train_loss, train_acc, test_loss, test_acc
+
+
 # -
 
 # Save and Visualize all results
@@ -256,22 +297,18 @@ def save_visualize(model, result, title):
     # save everything
     torch.save(model.state_dict(), f'results/{title}')
     torch.save(result, f'results/{title}_result')
-    # torch.save(train_loss,f'attacker/results/{title}_TrainLoss')
-    # torch.save(train_acc,f'attacker/results/{title}_TrainAcc')
-    # torch.save(test_loss,f'attacker/results/{title}_TestLoss' )
-    # torch.save(test_acc,f'attacker/results/{title}_TestAcc' )
 
     # plot
-    plt.plot(range(config["epochs"]), train_loss, 'b-', label='Train_loss')
-    plt.plot(range(config["epochs"]), test_loss, 'g-', label='Test_loss')
+    plt.plot(train_loss, 'b-', label='Train_loss')
+    plt.plot(test_loss, 'g-', label='Test_loss')
     plt.title(f'{title} - Loss')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.legend()
     plt.savefig(f'results/{title}_Loss')
     plt.show()
-    plt.plot(range(config["epochs"]), train_acc, 'b-', label='Train_accuracy')
-    plt.plot(range(config["epochs"]), test_acc, 'g-', label='Test_accuracy')
+    plt.plot(train_acc, 'b-', label='Train_accuracy')
+    plt.plot(test_acc, 'g-', label='Test_accuracy')
     plt.title(f'{title} - Accuracy')
     plt.xlabel('Epochs')
     plt.ylabel('Accuracy')
